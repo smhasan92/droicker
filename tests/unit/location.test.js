219fe1a3e41118557b5d6760744437e2461c1172
Hasan Tanveer <hasan.tanveer@veripark.com>
2022-05-24 19:55:00 +0500

    adding first test case

---
+++ tests/unit/location.test.js
@@ -0,0 +1,14 @@
+const location = require('../../controllers/location')
+
+describe('getLocation', () => {
+    test('getLocation() is present', () => {
+        expect(location.getLocation).toBeDefined();
+    });
+});
+
+
+describe('addLocation', () => {
+    test('addLocation() is present', () => {
+        expect(location.getLocation).toBeDefined();
+    });
+});
