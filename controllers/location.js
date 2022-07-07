88e65dab3627904d6a6761c7b04daaed825f1f8f
Hasan Tanveer <xpert_only@hotmail.com>
2022-07-07 15:08:36 +0500

    async handler

--- controllers/location.js
+++ controllers/location.js
@@ -1,41 +1,32 @@
 const ErrorResponse = require('../utils/errorHandler');
+const asyncHandler = require('../middleware/async');
 const Location = require('../models/Location');
 
 
 // @desc  Get all devices
 // @route GET /api/v1/devices
 // @access Public
-exports.getLocation = async (req, res, next) => {
-  try {
-    console.log("Call Start : getLocation".underline.blue)
-    const location = await Location.find();
-    console.log("Call End : getLocation".underline.blue)
-    return res.status(200).json({
-      success: true,
-      count: location.length,
-      data: location
-    });
-  } catch (err) {
-    next(new ErrorResponse(err.message,404));
-  }
-};
+exports.getLocation = asyncHandler(async (req, res, next) => {
+  console.log("Call Start : getLocation".underline.blue)
+  const location = await Location.find();
+  console.log("Call End : getLocation".underline.blue)
+
+  return res.status(200).json({
+    success: true,
+    count: location.length,
+    data: location
+  });
+});
 
 // @desc  Create a store
 // @route POST /api/v1/devices
 // @access Public
-exports.addLocation = async (req, res, next) => {
-  try {
-    console.log('addLocation Start'.underline.blue)
-    const location = await Location.create(req.body);
-    console.log('addLocation End'.underline.blue)
-    return res.status(201).json({
-      success: true,
-      data: location
-    });
-  } catch (err) {
-      if (err.code === 11000) {
-        next(new ErrorResponse('This location already exists',400));
-      }
-    next(err);
-  }
-};
+exports.addLocation = asyncHandler(async (req, res, next) => {
+  console.log('addLocation Start'.underline.blue)
+  const location = await Location.create(req.body);
+  console.log('addLocation End'.underline.blue)
+  return res.status(201).json({
+    success: true,
+    data: location
+  });
+});
